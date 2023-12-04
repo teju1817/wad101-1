3f9cea618a36f6380b1d0c4774cf88c18b051956
Nithin-2413 <135258263+Nithin-2413@users.noreply.github.com>
2023-12-05 01:53:51 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -3,45 +3,39 @@ const fs = require('fs');
 const path = require('path');
 const minimist = require('minimist');
 
+
 const args = minimist(process.argv.slice(2));
-const port = args.port || 3000; // Default port 3000 if not specified
+const port = args.port || 3000; // Default port is 3000 if not specified
 
-const server = http.createServer((req, res) => {
-  let filePath = '.' + req.url;
 
-  if (filePath === './') {
-    filePath = './home.html'; // Default home page
-  }
-
-  filePath = path.resolve(filePath);
-
-  const extname = String(path.extname(filePath)).toLowerCase();
-  const contentType = {
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.css': 'text/css',
-    '.json': 'application/json',
-    '.png': 'image/png',
-    '.jpg': 'image/jpg',
-    '.gif': 'image/gif',
-    '.svg': 'image/svg+xml',
-  }[extname] || 'application/octet-stream';
-
-  fs.readFile(filePath, (err, content) => {
+function serveHTMLFile(filePath, res) {
+  const fullPath = path.join(__dirname, filePath);
+  fs.readFile(fullPath, (err, content) => {
     if (err) {
-      if (err.code === 'ENOENT') {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('404 Not Found');
-      } else {
-        res.writeHead(500);
-        res.end(`Server Error: ${err.code}`);
-      }
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
     } else {
-      res.writeHead(200, { 'Content-Type': contentType });
-      res.end(content, 'utf-8');
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content);
     }
   });
+}
+
+const server = http.createServer((req, res) => {
+  const { url } = req;
+
+  if (url === '/' || url === '/home.html') {
+    serveHTMLFile('home.html', res);
+  } else if (url === '/project.html') {
+    serveHTMLFile('project.html', res);
+  } else if (url === '/registration.html') {
+    serveHTMLFile('registration.html', res);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
 });
+
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
