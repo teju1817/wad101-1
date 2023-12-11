7731e85e0d421ef05b97703a324f38e1f300a849
Nithin-2413 <135258263+Nithin-2413@users.noreply.github.com>
2023-12-12 01:40:09 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,50 +1,34 @@
-const TodoList = () => {
-  const allItems = [];
-
-  const addItem = (todoItem) => {
-    allItems.push(todoItem);
-  };
-
-  const markAsComplete = (index) => {
-    allItems[index].completed = true;
-  };
-
-  const formattedDate = (date) => {
-    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
-    return date.toLocaleDateString('en-US', options);
-  };
-
-  const getOverdue = () => {
-    const today = new Date().toISOString().split("T")[0];
-    return allItems.filter((item) => !item.completed && item.dueDate < today);
-  };
-
-  const getDueToday = () => {
-    const today = new Date().toISOString().split("T")[0];
-    return allItems.filter((item) => item.dueDate === today);
-  };
-
-  const getDueLater = () => {
-    const today = new Date().toISOString().split("T")[0];
-    return allItems.filter((item) => !item.completed && item.dueDate > today);
-  };
-
-  const toDisplayableList = () => {
-    const overdueList = getOverdue();
-    const dueTodayList = getDueToday();
-    const dueLaterList = getDueLater();
-
-    console.log(`My Todo-list\n`);
-    console.log(`Overdue\n${overdueList.map(item => `[ ] ${item.title} ${formattedDate(item.dueDate)}`).join('\n')}\n`);
-    console.log(`Due Today\n${dueTodayList.map(item => `[x] ${item.title}`).join('\n')}\n`);
-    console.log(`Due Later\n${dueLaterList.map(item => `[ ] ${item.title} ${formattedDate(item.dueDate)}`).join('\n')}`);
-  };
-
-  return {
-    addItem,
-    markAsComplete,
-    toDisplayableList,
-  };
+const todoList = {
+  all: [],
+  addItem: function (todoItem) {
+    this.all.push(todoItem);
+  },
+  markAsComplete: function (index) {
+    this.all[index].completed = true;
+  },
+  formattedDate: function (d) {
+    return d.toISOString().split("T")[0];
+  },
+  overdue: function () {
+    const today = new Date().toISOString().split('T')[0];
+    return this.all.filter(item => !item.completed && item.dueDate < today);
+  },
+  dueToday: function () {
+    const today = new Date();
+    const todayFormatted = this.formattedDate(today);
+    return this.all.filter(item => item.dueDate === todayFormatted);
+  },
+  dueLater: function () {
+    const today = new Date().toISOString().split('T')[0];
+    return this.all.filter(item => !item.completed && item.dueDate > today);
+  },
+  toDisplayableList: function () {
+    const today = new Date().toISOString().split('T')[0];
+    const displayableList = this.all.map(item =>
+      `${item.completed ? "[x]" : "[ ]"} ${item.title}${item.dueDate === today ? "" : " " + item.dueDate}`
+    ).join("\n");
+    console.log(displayableList);
+  }
 };
 
-module.exports = TodoList;
+module.exports = todoList;
