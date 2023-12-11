9f9216c3ce563d6b5ea5ff31db48da3fe812cca5
Nithin-2413 <135258263+Nithin-2413@users.noreply.github.com>
2023-12-12 01:37:59 +0530

    Update addTodo.js

--- todo-cli/addTodo.js
+++ todo-cli/addTodo.js
@@ -1,17 +1,18 @@
-const args = require("minimist")(process.argv.slice(2));
-const database = require("./models/index");
+const TodoList = require('./path_to_TodoList');
 
-const addTodo = async (params) => {
+const argv = require('minimist')(process.argv.slice(2));
+
+const createTodo = async (params) => {
   try {
-    await database.Todo.addTask(params);
+    TodoList.addItem(params);
   } catch (error) {
     console.error(error);
   }
 };
 
-const calculateDueDate = (days) => {
+const getJSDate = (days) => {
   if (!Number.isInteger(days)) {
-    throw new Error("Days should be an integer.");
+    throw new Error("Need to pass an integer as days");
   }
   const today = new Date();
   const oneDay = 60 * 60 * 24 * 1000;
@@ -19,19 +20,14 @@ const calculateDueDate = (days) => {
 };
 
 (async () => {
-  const { title, dueInDays } = args;
+  const { title, dueInDays } = argv;
   if (!title || dueInDays === undefined) {
     throw new Error(
-      'Both title and dueInDays are required. \nExample: node addTodo.js --title="Buy groceries" --dueInDays=2'
+      'title and dueInDays are required. \nSample command: node addTodo.js --title="Buy milk" --dueInDays=-2 '
     );
   }
-  
-  const dueDate = calculateDueDate(dueInDays);
-  await addTodo({
-    title,
-    dueDate,
-    completed: false
-  });
-
-  await database.Todo.showList();
+  const dueDate = getJSDate(dueInDays);
+  const completed = false;
+  await createTodo({ title, dueDate, completed });
+  TodoList.toDisplayableList();
 })();
